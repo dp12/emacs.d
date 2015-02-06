564c5a8e2572ee274651bb2cb6b169b61f54f538
dp12 <dp12@github.com>
2015-02-05 22:56:36 -0500

    Adjusted aliases and added mouse xterm mouse support

--- lisp/init-xterm.el
+++ lisp/init-xterm.el
@@ -23,4 +23,10 @@
                    (mwheel-install)))
             ))
 
+;; Enable mouse support
+(require 'mouse)
+(xterm-mouse-mode t)
+(defun track-mouse (e))
+(setq mouse-sel-mode t)
+
 (provide 'init-xterm)
