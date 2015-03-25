acbd863c83eec087d700252196dc47c577aa107b
dp12 <dp12@github.com>
2015-03-25 12:53:47 -0400

    Added firefox as default URL opener on Windows Added C src babel snippet

--- lisp/init-emacs-w3m.el
+++ lisp/init-emacs-w3m.el
@@ -48,7 +48,11 @@
        (*is-a-mac* "open")
        (*linux* (executable-find "firefox"))
        ))
-;; (setq browse-url-browser-function 'browse-url-generic)
+
+;; Set default browser to Firefox in Windows
+(if (eq system-type 'windows-nt)
+    (setq browse-url-browser-function 'browse-url-default-windows-browser)
+  (setq browse-url-browser-function 'browse-url-generic))
 
 ;; use external browser to search programming stuff
 (defun w3mext-hacker-search ()
